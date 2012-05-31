0abd62c1a01471b9894279941a3582975da603eb
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-05-31 09:42:23 -0700

    Correct path to where I keep my local copy of Clojure repo

--- src/clj_prescreen/core.clj
+++ src/clj_prescreen/core.clj
@@ -626,7 +626,7 @@ apply the patch, and try to build with 'ant' in that copy."
 (use 'clj-prescreen.core 'clojure.pprint)
 (require '[clojure.java.io :as io] '[fs.core :as fs])
 (def cur-eval-dir (str @fs/cwd "/eval-results/2012-05-31/"))
-(def clojure-tree "./2012-05-24-clojure-to-prescreen/clojure-plus-clj-967-patch")
+(def clojure-tree "./eval-results/2012-05-24-clojure-to-prescreen/clojure-plus-clj-967-patch")
 (def ticket-dir (str cur-eval-dir "ticket-info"))
 ;;(def patch-type-list [ "screened" "incomplete" "np" "rfs"])
 (def patch-type-list [ "notclosed" ])
