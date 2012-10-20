e32caeea62fe889907f8e74ad1138782c1d63d18
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-10-20 10:45:49 -0700

    Add patches, and update core.clj, for Oct 20, 2012

--- src/clj_prescreen/core.clj
+++ src/clj_prescreen/core.clj
@@ -632,8 +632,8 @@ apply the patch, and try to build with 'ant' in that copy."
 
 (use 'clj-prescreen.core 'clojure.pprint)
 (require '[clojure.java.io :as io] '[fs.core :as fs])
-(def cur-eval-dir (str @fs/cwd "/eval-results/2012-10-18/"))
-(def clojure-tree "./eval-results/2012-10-16-clojure-to-prescreen/clojure")
+(def cur-eval-dir (str @fs/cwd "/eval-results/2012-10-20/"))
+(def clojure-tree "./eval-results/2012-10-20-clojure-to-prescreen/clojure")
 ;;(def clojure-tree "./eval-results/2012-09-22-clojure-to-prescreen/clojure-plus-clj-967-patch")
 (def ticket-dir (str cur-eval-dir "ticket-info"))
 ;;(def patch-type-list [ "screened" "incomplete" "np" "rfs"])
