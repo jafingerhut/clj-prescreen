4209652475098eda850cb48f0eb2a50e4ac90c80
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-01-03 23:52:20 -0800

    Update Clojure version to 1.5.0-RC1 to test it a bit

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.5.0-RC1"]
                  [org.clojure/data.zip "0.1.0"]
                  [fs "1.1.2"]
                  ])
