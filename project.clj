557598c74bf08a66a71970212f16ee25a8ce11ba
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-01-21 21:07:54 -0800

    Test out Clojure 1.5.0-RC2, and add joda-time to dependencies to get its days between date calculating routines

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.5.0-RC1"]
+                 [org.clojure/clojure "1.5.0-RC2"]
                  [org.clojure/data.zip "0.1.0"]
                  [fs "1.1.2"]
                  [clj-http "0.6.3"]
+                 [joda-time "2.1"]
                  ])
