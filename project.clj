efac7353c31d1fbf501fa4d1fbf3eabea9f3a317
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-02-14 13:35:08 -0800

    Use tools.reader 0.7.0 for reading edn data safely

--- project.clj
+++ project.clj
@@ -2,10 +2,7 @@
   :description "FIXME: write description"
   :dependencies [
                  [org.clojure/clojure "1.5.0-RC16"]
-                 ;; tools.reader 0.6.5 has a bug that prevents it from
-                 ;; reading from a java.io.PushbackReader.  Wait for
-                 ;; next release, which should fix that.
-                 ;;[org.clojure/tools.reader "0.6.5"]
+                 [org.clojure/tools.reader "0.7.0"]
                  [org.clojure/data.zip "0.1.0"]
                  [fs "1.1.2"]
                  [clj-http "0.6.3"]
