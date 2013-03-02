cecc6194d9b232f9d73b9929800937ebb7ec41a6
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-03-02 01:00:18 -0800

    Add patches, and update core.clj, for Mar 1, 2013

--- src/clj_prescreen/core.clj
+++ src/clj_prescreen/core.clj
@@ -1211,8 +1211,8 @@ from most to fewest votes.
 
 (use 'clj-prescreen.core 'clojure.pprint)
 (require '[clojure.java.io :as io] '[fs.core :as fs])
-(def cur-eval-dir (str @fs/cwd "/eval-results/2013-02-28/"))
-(def clojure-tree "./eval-results/2013-02-22-clojure-to-prescreen/clojure")
+(def cur-eval-dir (str @fs/cwd "/eval-results/2013-03-01/"))
+(def clojure-tree "./eval-results/2013-03-01-clojure-to-prescreen/clojure")
 (def ticket-dir (str cur-eval-dir "ticket-info"))
 (def patch-type-list [ "open" ])
 ;; Note: Don't check any password into git
