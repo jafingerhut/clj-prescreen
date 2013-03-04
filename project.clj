1e8c96976805974f6623bb06cb9579c728c345c7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-03-04 15:44:32 -0800

    Update tools.reader dependency to latest version 0.7.2

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :dependencies [
                  [org.clojure/clojure "1.5.0"]
-                 [org.clojure/tools.reader "0.7.0"]
+                 [org.clojure/tools.reader "0.7.2"]
                  [org.clojure/data.zip "0.1.0"]
                  [fs "1.1.2"]
                  [clj-http "0.6.3"]
