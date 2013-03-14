9ab8ee68e7acf7ff03fb33e2a855c9f3954f84d9
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-03-14 15:45:46 -0700

    Update Clojure to 1.5.1, and also versions of tools.reader, fs, and data.zip

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.5.0"]
-                 [org.clojure/tools.reader "0.7.2"]
-                 [org.clojure/data.zip "0.1.0"]
-                 [fs "1.1.2"]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.7.3"]
+                 [org.clojure/data.zip "0.1.1"]
+                 [fs "1.3.3"]
                  [clj-http "0.6.3"]
                  [joda-time "2.1"]
                  ])
