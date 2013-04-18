11af4549460c8addc9359f6296a19083c01351d4
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-04-18 14:18:42 -0700

    Add patches, and update core.clj, for Apr 18, 2013

--- src/clj_prescreen/core.clj
+++ src/clj_prescreen/core.clj
@@ -1439,7 +1439,7 @@ Project %s tickets
 
 (use 'clj-prescreen.core 'clojure.pprint)
 (require '[clojure.java.io :as io] '[fs.core :as fs])
-(def cur-eval-dir (str fs/*cwd* "/eval-results/2013-04-11/"))
+(def cur-eval-dir (str fs/*cwd* "/eval-results/2013-04-18/"))
 (def clojure-tree "./eval-results/2013-04-13-clojure-to-prescreen/clojure")
 (def ticket-dir (str cur-eval-dir "ticket-info"))
 (def patch-type-list [ "open" ])
