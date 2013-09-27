ea3abc96093d07fc8f1fc9996c53e8008c183e21
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-09-26 19:18:31 -0700

    Back off from fs 1.4.4 to 1.4.3.  Had troubles with 1.4.4 and JDK6

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/tools.reader "0.7.7"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.trace "0.7.6"]
-                 [me.raynes/fs "1.4.4"]
+                 [me.raynes/fs "1.4.3"]
                  [clj-http "0.7.7"]
                  [joda-time "2.1"]
                  ]
