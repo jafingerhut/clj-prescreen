5c6e040968fab813de31d2c89fa9f0591d891d05
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-11-22 17:35:58 -0800

    Update list of Clojure JIRA users

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.7.7"]
+                 ;;[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.6.0-alpha2"]
+                 [org.clojure/tools.reader "0.7.10"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.trace "0.7.6"]
                  [me.raynes/fs "1.4.3"]
