6f8fdee5855c0586e267637f5db72d3b816df25e
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-03-26 10:22:31 -0700

    Update to Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 ;;[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure "1.6.0-RC1"]
+                 [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.reader "0.7.10"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.trace "0.7.6"]
