995cfb375bf858167d3ffed925f3e34d0704a0d8
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2016-08-19 22:53:32 -0700

    Upgrade Clojure version to 1.8.0 and most/all dependency versions to latest available

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.7.10"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [org.clojure/tools.trace "0.7.6"]
-                 [me.raynes/fs "1.4.3"]
-                 [clj-http "0.7.7"]
-                 [joda-time "2.1"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.reader "0.10.0"]
+                 [org.clojure/data.zip "0.1.2"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 [me.raynes/fs "1.4.6"]
+                 [clj-http "3.1.0"]
+                 [joda-time "2.9.4"]
                  ]
   :jvm-opts ^:replace ["-Xmx1024m"]
   :main clj-prescreen.core)
