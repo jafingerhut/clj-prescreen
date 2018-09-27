0201fe139369dd040ca692f2f8550ebdcabb492e
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-09-27 00:41:16 -0700

    Update deps.  Add test-all alias

--- project.clj
+++ project.clj
@@ -1,13 +1,21 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.reader "0.10.0"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.reader "1.3.0"]
                  [org.clojure/data.zip "0.1.2"]
-                 [org.clojure/tools.trace "0.7.9"]
                  [me.raynes/fs "1.4.6"]
-                 [clj-http "3.1.0"]
-                 [joda-time "2.9.4"]
+                 [clj-http "3.9.1"]
+                 [joda-time "2.10"]
                  ]
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha8"]]}}
+  :aliases {"test-all" ["with-profile"
+                        "dev,test,1.6:dev,test,1.7:dev,test,1.8:dev,test,1.9:dev,test,1.10"
+                        "test"]}
   :jvm-opts ^:replace ["-Xmx1024m"]
   :main clj-prescreen.core)
