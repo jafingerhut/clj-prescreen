c2aae6e813093b9addc24c6154a9c80dd8314639
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-12-27 12:09:34 -0800

    Update to use Clojure 1.10.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-prescreen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.reader "1.3.0"]
                  [org.clojure/data.zip "0.1.2"]
                  [me.raynes/fs "1.4.6"]
